f39c247b426e9a7d9711714dd9471c9579ccff36
JulienCesari <150373877+JulienCesari@users.noreply.github.com>
2023-11-30 19:49:00 +0100

    Update Transmigration.java

---
+++ karmaka/src/fr/utt/karmaka/Transmigration.java
@@ -0,0 +1,12 @@
+package fr.utt.karmaka;
+
+public class Transmigration extends Carte {
+
+	public Transmigration() {
+		super(0, 0, 1, "Placez dans votre main n'importe quelle carte de votre Vie Future", "Transmigration");
+	}
+	
+	public void activerCapacité(Carte carte) {
+		
+	}
+}
